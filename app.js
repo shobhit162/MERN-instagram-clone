9883086a4297fb6574626dc3dfe6035071ec1f88
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2023-03-25 16:41:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURL} = require('./config/prod')
+const {MONGOURL} = require('./config/keys')
+const cors = require('cors')
 
 
 
@@ -26,13 +27,20 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+// if(process.env.NODE_ENV=="production"){
+//     app.use(express.static('client/build'))
+//     const path = require('path')
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
+
+app.use(express.json());
+app.use(cors());
+app.get("/",(req,res) =>{
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.send("API is running..");
+});
 
 
 app.listen(PORT,()=>{
